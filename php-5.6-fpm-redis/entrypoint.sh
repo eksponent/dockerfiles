36e6c8f3af4712e663f163915db694e3ce4974a6
Andreas Elmertoft <andreas.elmertoft@obrigado.dk>
2016-04-01 16:27:39 +0200

    Add comment

--- php-5.6-fpm-redis/entrypoint.sh
+++ php-5.6-fpm-redis/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Umask should be set to 002 or group write will be disabled
+# Quite a few installs run into an issue where UMASK 022 should be 002 so its closer to 0775
+# https://hub.docker.com/r/nubs/composer-build/~/dockerfile/
 umask 002
 
 # Run php-fpm
