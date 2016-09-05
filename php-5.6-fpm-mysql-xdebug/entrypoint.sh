3c1cf463fa64610d97951f7d7c29fe1da66fe0ed
Leigh Morresi <dgtlmoon@gmail.com>
2016-09-05 12:57:29 +0200

    Remove connect_back so it always connects back to 127.0.0.1:9000 (can be used for debugging behat sessions then)

--- php-5.6-fpm-mysql-xdebug/entrypoint.sh
+++ php-5.6-fpm-mysql-xdebug/entrypoint.sh
@@ -15,7 +15,6 @@ if [ -n "$ENABLE_XDEBUG" ]; then
         && sed -i '1 a xdebug.remote_autostart=true' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
         && sed -i '1 a xdebug.remote_mode=req' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
         && sed -i '1 a xdebug.remote_handler=dbgp' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
-        && sed -i '1 a xdebug.remote_connect_back=1 ' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
         && sed -i '1 a xdebug.remote_port=9000' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
         && sed -i '1 a xdebug.remote_host=127.0.0.1' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
         && sed -i '1 a xdebug.remote_enable=1' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
