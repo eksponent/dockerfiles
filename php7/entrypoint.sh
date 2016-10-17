f7224dbd0eaa174319aeaea3a88fb7180f5f0cce
Leigh Morresi <dgtlmoon@gmail.com>
2016-10-17 12:26:43 +0200

    Adding initial PHP7+redis+xdebug

---
+++ php7/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Umask should be set to 002 or group write will be disabled
+# Quite a few installs run into an issue where UMASK 022 should be 002 so its closer to 0775
+# https://hub.docker.com/r/nubs/composer-build/~/dockerfile/
+umask 002
+
+# Enable xdebug if the ENABLE_XDEBUG environment variable is set
+# We only want xdebug in specific circumstances so it's better to opt-in when we need it
+if [ -n "$ENABLE_XDEBUG" ]; then
+    # Install and configure Xdebug, RUN is not used in entrypoint scripts
+    pecl install xdebug \
+        && docker-php-ext-enable xdebug \
+        && touch /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+        && sed -i '1 a xdebug.remote_autostart=true' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+        && sed -i '1 a xdebug.remote_mode=req' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+        && sed -i '1 a xdebug.remote_handler=dbgp' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+        && sed -i '1 a xdebug.remote_port=9000' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+        && sed -i '1 a xdebug.remote_connect_back=1' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+        && sed -i '1 a xdebug.remote_enable=1' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+fi
+
+# Run php-fpm
+php-fpm
