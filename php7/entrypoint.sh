434afb2d44e57371771e4982275013d27a096527
Leigh Morresi <dgtlmoon@gmail.com>
2018-06-21 13:51:18 +0200

    support for forced connetc back

--- php7/entrypoint.sh
+++ php7/entrypoint.sh
@@ -20,5 +20,20 @@ if [ -n "$ENABLE_XDEBUG" ]; then
         && sed -i '1 a xdebug.remote_enable=1' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 fi
 
+# Enable xdebug if the ENABLE_XDEBUG environment variable is set
+# We only want xdebug in specific circumstances so it's better to opt-in when we need it
+if [ -n "$ENABLE_XDEBUG_FORCEHOST" ]; then
+    # Install and configure Xdebug, RUN is not used in entrypoint scripts
+    pecl install xdebug \
+        && docker-php-ext-enable xdebug \
+        && touch /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+        && sed -i '1 a xdebug.remote_autostart=true' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+        && sed -i '1 a xdebug.remote_mode=req' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+        && sed -i '1 a xdebug.remote_handler=dbgp' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+        && sed -i '1 a xdebug.remote_port=9000' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+        && sed -i "1 a xdebug.remote_host=$ENABLE_XDEBUG_FORCEHOST" /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+        && sed -i '1 a xdebug.remote_enable=1' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+fi
+
 # Run php-fpm
 php-fpm
